b49307bce865d1896256cd386b333e29e4c1e91b
KarinaT <tarasenkoka@gmail.com>
2013-11-13 17:44:45 +0200

    Update WebDriverFactory.java

--- FrameworkBuild/src/main/java/com/epam/preproduction/helpers/core/WebDriverFactory.java
+++ FrameworkBuild/src/main/java/com/epam/preproduction/helpers/core/WebDriverFactory.java
@@ -1,18 +1,21 @@
 package com.epam.preproduction.helpers.core;
+
 import java.net.MalformedURLException;
 import java.net.URL;
+
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+
 import com.epam.preproduction.configuration.PropertyReader;
 import com.opera.core.systems.OperaDriver;
 
 public class WebDriverFactory {
+
 	private static String defaultHub = null;
 
 	private static int restartFrequency = Integer.MAX_VALUE;
@@ -20,9 +23,11 @@ public class WebDriverFactory {
 	public static void setDefaultHub(String newDefaultHub) {
 		defaultHub = newDefaultHub;
 	}
+
 	public static void setRestartFrequency(int newRestartFrequency) {
 		restartFrequency = newRestartFrequency;
 	}
+
 	private static String key = null;
 	private static int count = 0;
 	private static WebDriver driver;
@@ -40,21 +45,26 @@ public class WebDriverFactory {
 			key = newKey;
 			return newWebDriver(hub, capabilities);
 		}
+
 		try {
 			driver.getCurrentUrl();
 		} catch (Throwable t) {
 			t.printStackTrace();
 			return newWebDriver(hub, capabilities);
 		}
+
 		if (count >= restartFrequency) {
 			dismissDriver();
 			return newWebDriver(hub, capabilities);
 		}
+
 		return driver;
 	}
+
 	public static WebDriver getDriver(Capabilities capabilities) {
 		return getDriver(defaultHub, capabilities);
 	}
+
 	public static void dismissDriver() {
 		if (driver != null) {
 			try {
@@ -66,6 +76,7 @@ public class WebDriverFactory {
 			}
 		}
 	}
+
 	private static WebDriver newWebDriver(String hub, Capabilities capabilities) {
 		driver = (hub == null) ? createLocalDriver(capabilities)
 				: createRemoteDriver(hub, capabilities);
@@ -73,6 +84,7 @@ public class WebDriverFactory {
 		count = 0;
 		return driver;
 	}
+
 	private static WebDriver createRemoteDriver(String hub,
 			Capabilities capabilities) {
 		try {
@@ -82,6 +94,7 @@ public class WebDriverFactory {
 			throw new Error("Could not connect to WebDriver hub", e);
 		}
 	}
+
 	private static WebDriver createLocalDriver(Capabilities capabilities) {
 		String browserType = capabilities.getBrowserName();
 		if (browserType.equals("firefox"))
@@ -93,11 +106,17 @@ public class WebDriverFactory {
 			return new ChromeDriver(capabilities);
 		}
 		if (browserType.equals("opera")) {
-			System.setProperty("opera.binary",PropertyReader.getOperaBinaryPath());
+
+			// DesiredCapabilities capabilities2 = DesiredCapabilities.opera();
+			// capabilities2.setCapability("opera.binary",
+			// PropertyReader.getOperaBinaryPath());
+			System.setProperty("opera.binary",
+					PropertyReader.getOperaBinaryPath());
 			return new OperaDriver(capabilities);
 		}
 		throw new Error("Unrecognized browser type: " + browserType);
 	}
+
 	static {
 		Runtime.getRuntime().addShutdownHook(new Thread() {
 			public void run() {
