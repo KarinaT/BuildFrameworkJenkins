41a82ccd8765a0468ee473911c074f128bc381e8
KarinaT <tarasenkoka@gmail.com>
2013-11-14 13:23:35 +0200

    Update DataProviderLayer.java

--- FrameworkBuild/src/main/java/com/epam/preproduction/configuration/DataProviderLayer.java
+++ FrameworkBuild/src/main/java/com/epam/preproduction/configuration/DataProviderLayer.java
@@ -5,15 +5,13 @@ import java.lang.reflect.Method;
 
 import org.testng.annotations.DataProvider;
 
-import com.epam.preproduction.helpers.core.ExcelReader;
-
 public class DataProviderLayer {
 	@DataProvider()
 	public static Object[][] readFromExcel(Method method) throws IOException {
 		String fileName = getDataFileName(method);
 		String sheetName = method.getName();
 		ExcelReader read = new ExcelReader();
-		return read.reader(fileName, sheetName);
+		return read.readExcel(fileName, sheetName);
 	}
 
 	private static String getDataFileName(Method method) {
